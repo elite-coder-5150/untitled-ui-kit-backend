70a656ca2df27e20adfefe8c93bfcab9a8fb183a
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-19 17:35:34 -0600

    the newsletter model

---
+++ models/Newsletter.js
@@ -0,0 +1,21 @@
+import { Mongoose } from "mongoose";
+import { Schema } from "mongoose";
+import { Express, Router } from "express";
+
+const NewsletterSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const Newsletter = Mongoose.model("Newsletter", NewsletterSchema);
