0ef476a6700ea82594ef572182ecbe4ff54afe78
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-19 17:45:00 -0600

    added the body field

--- models/Notifications.js
+++ models/Notifications.js
@@ -1,2 +1,15 @@
 import { Mongoose } from "mongoose";
 import { Schema } from "mongoose";
+
+const NotificationSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+});
+
+const Notification = Mongoose.model("Notification", NotificationSchema);
