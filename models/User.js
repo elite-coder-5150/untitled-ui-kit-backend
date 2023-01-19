9f1b93af71b8b0334b76156ef1a3de27fc822535
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-19 17:43:35 -0600

    defined the user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+import { Mongoose, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    joinedDate: {
+        type: Date,
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now
+    },
+
+    social: {
+        facebook: {
+            type: String
+        },
+
+        twitter: {
+            type: String
+        },
+
+        github: {
+            type: String
+        },
+
+        linkedin: {
+            type: String
+        },
+
+        youtube: {
+            type: String
+        },
+
+    }
+});
+
+const User = Mongoose.model("User", UserSchema);
