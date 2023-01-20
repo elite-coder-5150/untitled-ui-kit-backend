2db78d2df23c2b374fc9ffb4fb801f493322f48f
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-19 18:10:03 -0600

    added the id field to the notifications model

--- models/Notifications.js
+++ models/Notifications.js
@@ -2,6 +2,10 @@ import { Mongoose } from "mongoose";
 import { Schema } from "mongoose";
 
 const NotificationSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
     title: {
         type: String,
         required: true
